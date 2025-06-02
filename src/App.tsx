f1c4d72ff22098474c785be709e3d9db0013e741
unknown <cocfanlike@gmail.com>
2025-06-02 20:52:32 +0545

    Set up TS in react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1 className = "text-3xl bg-amber-500">Hello World This is on Typescript</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
